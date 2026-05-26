328cda8e3d063d70d5f65a4ce138c1cd9b8f8753
Zolkiewicz <zolkiewicz2005@gmail.com>
2026-05-26 22:23:20 +0200

    refactor: refactor most of code for future handling background arguments reafctor: move line and tokens to global var

--- include/config.h
+++ include/config.h
@@ -2,6 +2,16 @@
 #define CONFIG_H
 
 #define LINE_LEN_BUFSIZE 1024
-#define TOKENS_BUFSIZE 16
+#define TOKENS_SIZE 16
+
+#define FOREGROUND 0
+#define BACKGROUND 1
+
+#define EOL 1
+#define ARG 2
+#define AMPERSAND 3
+#define SEMICOLON 4
+
+static char special [] = {' ', '\t', '&', ';', '\n', '\0'};
 
 #endif //CONFIG_H
--- src/main.c
+++ src/main.c
@@ -4,19 +4,27 @@
 #include <string.h>
 #include "config.h"
 #include "exec.h"
+#include <stdbool.h>
+
+char* line = NULL;
+char** tokens = NULL;
 
 /*** declarations ***/
 void clear_screen(void);
 void main_loop(void);
-char* shell_read_line(void);
-char** shell_split_line(const char*, int*);
+void shell_read_line(void);
+int shell_get_tok(size_t* len, char* input);
+bool is_arg_char(char c);
+void shell_split_line(void);
 
 /*** init ***/
 int main(void) {
     clear_screen();
-    while(1)
+    
+    while(1) {
         main_loop();
-
+    }
+        
     return 0; 
 }
 
@@ -27,66 +35,122 @@ void clear_screen(void) {
 
 /*** main_loop ***/
 void main_loop(void) {
-    char* line = NULL;
-    char** tokens = NULL;
-    int arg = 0;
     shell_info();
     printf("> ");
-    line = shell_read_line();
+    fflush(stdout);
+
+    shell_read_line();
+
     if (!line) {
         printf("\n");
         exit(EXIT_SUCCESS);
     }
-    tokens = shell_split_line(line, &arg);
-    shell_execution(tokens);
 
-    for (int i = 0; i < arg; i++) free(tokens[i]);
+    shell_split_line();
 
+    if (tokens != NULL) {
+        for (int i = 0; tokens[i] != NULL; i++) {
+            free(tokens[i]);
+        }
+        free(tokens);
+        tokens = NULL;
+    }
+    
     free(line);
-    free(tokens);
+    line = NULL;
 }
 
 /*** input ***/
-char* shell_read_line(void) {
+
+void shell_read_line(void) {
     size_t buf_size = LINE_LEN_BUFSIZE;
-    char* buffer = malloc(buf_size * sizeof(char));
-    if (buffer == NULL) fatal("Allocation error.");
 
-    if (getline(&buffer, &buf_size, stdin) == -1) {
-        free(buffer);
-        return NULL;
+    if (getline(&line, &buf_size, stdin) == -1) {
+        free(line);
+        line = NULL;
     }
-
-    return buffer;
 }
 
-char** shell_split_line(const char* line, int* arg) {
-    int buf_size = TOKENS_BUFSIZE;
-    int i = 0;
+int shell_get_tok(size_t* len, char* input) {
+    *len = 0;
 
-    char** buffer_tok =  malloc(buf_size * sizeof(char*));
-    if (buffer_tok == NULL) fatal("Allocation error");
+    switch (*input) {
+        case '\n':
+            return EOL;
+        case '&':
+            return AMPERSAND;
+        case ';':
+            return SEMICOLON;
+        default:
+            (*len)++;
+            while (is_arg_char(input[*len])) 
+                (*len)++;
+            break;
 
-    char* line_copy = strdup(line);
-    if (line_copy == NULL) fatal("Allocation error");
-
-    char delimiter[] = " \t\r\n\a";
-
-    char* token = strtok(line_copy, delimiter);
-    while (token) {
-        buffer_tok[i] = strdup(token);
-
-        if (++i >= buf_size) {
-            buf_size += TOKENS_BUFSIZE;
-            buffer_tok = realloc(buffer_tok, buf_size * sizeof(char*));
-            if (buffer_tok == NULL) fatal("Allocation error");
-        }
-        token = strtok(NULL, delimiter);
     }
-    buffer_tok[i] = NULL;
-    *arg = i;
-    free(line_copy);
-    return buffer_tok;
+    return ARG;
+}
+
+bool is_arg_char(char c) {
+    char *wrk;
+    for (wrk = special; *wrk; wrk++)
+        if (c == *wrk)
+            return false;
+
+    return true;
+}
+
+void shell_split_line(void) {
+    size_t len = 0;
+    char* pos = line; 
+    size_t size = TOKENS_SIZE;
+    size_t arg = 0;
+    int type;
+    int tok_type;
+
+    tokens =  malloc(size * sizeof(char*));
+    if (tokens == NULL) 
+        fatal("Allocation error");
+    tokens[0] = NULL;
+
+    while (pos != NULL) {
+        if (*pos == ' ' || *pos == '\t') {
+            pos++;
+            continue;
+        }
+
+        switch ((tok_type = shell_get_tok(&len, pos))) {
+            case ARG:
+                if (arg >= size - 1) {
+                    size += TOKENS_SIZE;
+                    tokens = realloc(tokens, size * sizeof(char*));
+                    if (tokens == NULL) fatal("Allocation error");
+                } 
+                tokens[arg] =  malloc((len + 1) * sizeof(char));
+                if (tokens[arg] == NULL) fatal("Allocation error");
+
+                memcpy(tokens[arg], pos, len);
+                tokens[arg][len] = '\0';
+                pos += len;
+                arg++;
+                tokens[arg] = NULL;
+                break;
+            case AMPERSAND:
+                if (tok_type == AMPERSAND) type = BACKGROUND;
+                else type = FOREGROUND;
+            case EOL:
+                if (EOL) pos = NULL;
+                else pos++;
+            case SEMICOLON:
+                if (arg != 0) {
+                    tokens[arg] = NULL;
+                    shell_execution(tokens);
+                }
+
+                arg = 0;
+                break;
+        }
+    }
 }
 
 
